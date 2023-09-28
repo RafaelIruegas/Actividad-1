2d1d619edd90ba573b43dbbac0c629f883aafba1
rafae <rafae@LAP-RAFA>
2023-09-28 15:11:44 -0600

    Agregar archivos de proyecto.

---
+++ Actividad 1/Controlers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Actividad_1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad_1.Controlers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+			MiPerfilViewModel vm = new();
+			vm.nombre = "Rafael Alejandro Iruegas Rosales";
+			return View(vm);
+        }
+        public IActionResult MiPerfil() { MiPerfilViewModel vm = new(); 
+            vm.nombre = "Rafael Alejandro Iruegas Rosales";
+            vm.edad = 21; 
+            vm.numerodecontrol="201G0263";
+            vm.Fecha = "01 / 15 / 2002";
+            vm.Correo = "rafaeliruegas1099@gmail.com";
+            return View(vm); }
+    }
+}
---
+++ Actividad 1/Models/MiPerfilViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Actividad_1.Models
+{
+    public class MiPerfilViewModel
+    {
+        public string nombre { get; set; } = null!;
+        public int edad { get; set; }
+        public string numerodecontrol { get; set; } = null!;
+        public string Fecha { get; set; } = null!;
+        public string Correo { get; set; } = null!;
+
+    }
+}
